398d870d64433a98e277f6b7d2c271af4de0558f
elmehdinainia <mehdinainiaa4@gmail.com>
2022-11-17 09:36:11 +0100

    strecture

--- tesst/src/components/Admin.jsx
+++ tesst/src/components/Admin.jsx
@@ -4,10 +4,17 @@ import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 
 const role =localStorage.getItem('role');
+const logout = () =>{
+    localStorage.clear()
+    window.location.replace('/');
+
+  }
 function Admin() {
 
   return(
+    
     role === "admin"? 
+
     <div>
     <section className="flex w-screen">
         <div className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800 w-64 h-screen">
@@ -36,10 +43,9 @@ function Admin() {
               
           
                 <li>
-                    <Link to="" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" /></svg>
-                    <span className="flex-1 ml-3 whitespace-nowrap">lougout</span>
-                    </Link>
+                <button className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button" onClick={logout}>
+                  logoute
+                </button>
                 </li>
             
             </ul>
